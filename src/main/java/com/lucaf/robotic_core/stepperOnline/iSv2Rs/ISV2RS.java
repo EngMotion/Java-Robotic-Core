513e4a1917384335e8123501f621be42c9b20d7d
luca <admin@lucaservers.com>
2026-08-30 18:06:12 +0200

    Remove peakCurrent from ISV

--- src/main/java/com/lucaf/robotic_core/stepperOnline/iSv2Rs/ISV2RS.java
+++ src/main/java/com/lucaf/robotic_core/stepperOnline/iSv2Rs/ISV2RS.java
@@ -4,10 +4,24 @@ import com.lucaf.robotic_core.State;
 import com.lucaf.robotic_core.dataInterfaces.impl.RegisterInterface;
 import com.lucaf.robotic_core.stepperOnline.iDmRs.IDMRS;
 
+import java.io.IOException;
 import java.util.HashMap;
+
+import static com.lucaf.robotic_core.stepperOnline.iDmRs.Constants.PEAK_CURRENT;
 
 
 public class ISV2RS extends IDMRS {
+
+    @Override
+    public void setPeakCurrent(int peakCurrent) throws IOException {
+        // Do nothing
+    }
+
+    @Override
+    public int getPeakCurrent() throws IOException {
+        return 0;
+    }
+
     public ISV2RS(RegisterInterface registerInterface, HashMap<String, Object> state, State notifyStateChange) {
         super(registerInterface, state, notifyStateChange);
     }
