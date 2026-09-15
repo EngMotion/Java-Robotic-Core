00d411d534dace4482601ab4d134913e471d240f
JureZivec <zivecj@gmail.com>
2026-09-15 08:14:05 +0200

    Revert linePowered and RTS/DTR changes from today

--- src/main/java/com/lucaf/robotic_core/SerialParams.java
+++ src/main/java/com/lucaf/robotic_core/SerialParams.java
@@ -2,12 +2,14 @@ package com.lucaf.robotic_core;
 
 import de.exlll.configlib.Configuration;
 import jssc.SerialPort;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 @NoArgsConstructor
 @Configuration
 public class SerialParams {
@@ -17,17 +19,6 @@ public class SerialParams {
     protected int databits = SerialPort.DATABITS_8;
     protected int stopbits = SerialPort.STOPBITS_1;
     protected int parity = SerialPort.PARITY_NONE;
-    protected boolean linePowered = false;
-
-    // Ohranjen star konstruktor s 6 parametri, da se ne pokvarijo obstoječi klici
-    public SerialParams(String comPort, int unitId, int baudrate, int databits, int stopbits, int parity) {
-        this.comPort = comPort;
-        this.unitId = unitId;
-        this.baudrate = baudrate;
-        this.databits = databits;
-        this.stopbits = stopbits;
-        this.parity = parity;
-    }
 
     public SerialParams(int baudrate, int databits, int stopbits, int parity) {
         this.baudrate = baudrate;
